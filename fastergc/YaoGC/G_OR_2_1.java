4b9a8eeed03be6ec20e13de297f79eb5349d26ea
Boyoung- <byaweics@gmail.com>
2016-02-15 16:39:39 -0800

    first commit

---
+++ fastergc/YaoGC/G_OR_2_1.java
@@ -0,0 +1,24 @@
+// Copyright (C) 2010 by Yan Huang <yhuang@virginia.edu>
+
+package YaoGC;
+
+class G_OR_2_1 extends OR_2_1 {
+	public G_OR_2_1() {
+		super();
+	}
+
+	protected void execYao() {
+		// fillTruthTable();
+		// encryptTruthTable();
+		// sendGTT();
+		// gtt = null;
+	}
+
+	@Override
+	protected void passTruthTable() {
+		fillTruthTable();
+		encryptTruthTable();
+		sendGTT();
+		gtt = null;
+	}
+}
---
+++ fastergc/YaoGC/G_T2_FF10_S_2_1.java
@@ -0,0 +1,24 @@
+// by Boyang Wei
+
+package YaoGC;
+
+class G_T2_FF10_S_2_1 extends T2_FF10_S_2_1 {
+	public G_T2_FF10_S_2_1() {
+		super();
+	}
+
+	protected void execYao() {
+		// fillTruthTable();
+		// encryptTruthTable();
+		// sendGTT();
+		// gtt = null;
+	}
+
+	@Override
+	protected void passTruthTable() {
+		fillTruthTable();
+		encryptTruthTable();
+		sendGTT();
+		gtt = null;
+	}
+}
