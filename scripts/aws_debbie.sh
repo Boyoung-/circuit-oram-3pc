84b434cfe2f5b75cbe8995068b4b4574503cff97
Ubuntu <ubuntu@ip-172-31-17-199.us-west-2.compute.internal>
2016-05-16 23:37:03 +0000

    correct error in scripts

--- scripts/aws_debbie.sh
+++ scripts/aws_debbie.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "${DIR}/../bin:${DIR}/../lib/*" -protocol rtv -eddie_ip 52.11.141.98 eddie
+java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol rtv -eddie_ip 52.11.141.98 eddie
