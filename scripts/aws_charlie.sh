f61faba2445ec1fa8433cfc48fee084e4946bb48
Boyoung- <byaweics@gmail.com>
2016-05-17 14:02:42 -0700

    update aws ips

--- scripts/aws_charlie.sh
+++ scripts/aws_charlie.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol rtv -eddie_ip 52.38.200.122 -debbie_ip 52.40.34.110 charlie
+java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol rtv -eddie_ip 52.32.4.179 -debbie_ip 52.40.35.23 charlie
--- scripts/aws_debbie.sh
+++ scripts/aws_debbie.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol rtv -eddie_ip 52.38.200.122 debbie
+java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol rtv -eddie_ip 52.32.4.179 debbie
