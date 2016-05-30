43d7a8712491cec558effe79cb693066acc3e907
Boyoung- <byaweics@gmail.com>
2016-05-29 19:18:06 -0700

    update scripts to pipeline

--- scripts/aws_charlie.sh
+++ scripts/aws_charlie.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol rtv -eddie_ip 52.40.13.95 -debbie_ip 52.39.207.59 charlie
+java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol rtv -eddie_ip  -pipeline -debbie_ip 52.38.139.230 charlie -pipeline
