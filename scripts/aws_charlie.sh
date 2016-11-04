22c6969d06261d71fd076866547293a0c6fbf773
Boyoung- <byaweics@gmail.com>
2016-11-04 11:36:57 -0700

    change default stash size to 32 in config; update aws ips

--- scripts/aws_charlie.sh
+++ scripts/aws_charlie.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol rtv -eddie_ip 52.42.255.187 -debbie_ip 52.38.255.37 charlie
+java -cp "${DIR}/../bin:${DIR}/../lib/*" ui.CLI -protocol rtv -eddie_ip 35.161.54.54 -debbie_ip 35.162.185.55 charlie
